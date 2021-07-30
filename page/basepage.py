e90a2b323eadfc9a17763e1af06921307c06a151
guiming <guiming1988@126.com>
2021-07-30 17:56:24 +0800

    20210730 V0.3

--- page/basepage.py
+++ page/basepage.py
@@ -1,7 +1,7 @@
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
-
+#temp
 
 class BasePage(object):
 
